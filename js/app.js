9b91502320c76107a55a03a5b50690ae9abfca2b
Nelson Devops <nelsonvicvera@gmail.com>
2023-05-17 16:41:57 -0500

    Update app.js
    Change in the code to make a gradually changing background collor

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ alert('Bienvenid@ !');
 
 var temporizador = setInterval(function () {
       setColor();
-}, 1000);
+}, 500);
 
 //function setColor() {
 //    var pagina = document.body;
@@ -16,30 +16,33 @@ function setColor() {
       var pagina = document.body;
       var currentColor = pagina.style.backgroundColor;
       var rgbValues = currentColor.match(/\d+/g);
-      var llegolimiteinf= true;
-      var llegolimitesup= false;
-      var newColor = "rgb(0, 0, 0)";
-      if (llegolimitesup= false && llegolimiteinf= true) {
-            if(rgbValues[2] <=255 ){
-                  var newColor = "rgb(" + rgbValues[0] + ", " + rgbValues[1] + ", " + (parseInt(rgbValues[2]) + 1) + ")";
-            }
-            if (rgbValues[2] = 255 ) {
-                  llegolimitesup= true
-                  llegolimiteinf= false
-            }
+      var llegolimiteinf = true;
+      var llegolimitesup = false;
+      
+      if (!llegolimitesup && llegolimiteinf) {
+        if (rgbValues[2] <= 255) {
+          rgbValues[2] = parseInt(rgbValues[2]) + 1;
+        }
+        if (rgbValues[2] == 255) {
+          llegolimitesup = true;
+          llegolimiteinf = false;
+        }
       }
-      if (llegolimiteinf= false && llegolimitesup = true) {
-            if(rgbValues[2] >= 0 ){
-                  var newColor = "rgb(" + rgbValues[0] + ", " + rgbValues[1] + ", " + (parseInt(rgbValues[2]) - 1) + ")";
-            }
-            if (rgbValues[2]= 0 ){
-                  llegolimiteinf= true
-                  llegolimitesup= false
-            }
+      
+      if (!llegolimiteinf && llegolimitesup) {
+        if (rgbValues[2] >= 0) {
+          rgbValues[2] = parseInt(rgbValues[2]) - 1;
+        }
+        if (rgbValues[2] == 0) {
+          llegolimiteinf = true;
+          llegolimitesup = false;
+        }
       }
+      
+      var newColor = "rgb(" + rgbValues[0] + ", " + rgbValues[1] + ", " + rgbValues[2] + ")";
       pagina.style.backgroundColor = newColor;
     }
-
+    
     
  //function stopChangeColor() {
 //    clearInterval(temporizador)
